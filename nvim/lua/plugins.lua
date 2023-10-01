c33851ef012a7a29b5d8c051c7ba73c7ab74b06c
Hadeed Ahmad <hadeedji@gmail.com>
2023-10-02 04:08:16 +0500

    Add nvimgdb

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,6 +21,24 @@ return {
     end
   },
 
+  {
+    "sakhnik/nvim-gdb",
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "nvimgdb",
+        callback = function()
+          vim.opt.number = false
+          vim.opt.relativenumber = false
+          vim.opt.signcolumn = 'no'
+
+          vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+            command = "startinsert"
+          })
+        end
+      })
+    end
+  },
+
   { "abecodes/tabout.nvim", dependencies = { "nvim-treesitter" }, config = true },
 
   { "numToStr/Comment.nvim",     config = true },
