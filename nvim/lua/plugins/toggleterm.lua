c81d4c0bd8611a4407da9e7a405caa8b59d2f6e8
Hadeed Ahmad <hadeedji@gmail.com>
2024-06-28 12:17:58 +0500

    Changes

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,13 +2,6 @@ return {
   "akinsho/toggleterm.nvim",
   opts = {
     open_mapping = "<C-t>",
-    direction = "float",
-    size = function(term)
-      if term.direction == "vertical" then
-        return 80
-      else
-        return 12
-      end
-    end
+    direction = "float"
   }
 }
